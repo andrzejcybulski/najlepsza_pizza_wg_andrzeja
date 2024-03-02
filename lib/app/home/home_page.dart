051adfd7c72679a2bc326292c02f8a6a176101d4
Andrzej Cybulski <andrzej_cybulski@o2.pl>
2024-03-02 14:47:14 +0100

    Implement CloudFirestore

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -19,35 +20,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Builder(
+          builder: (context) {
+            if (currentIndex == 0) {
+              return const Text('Najlepsza pizza wg Andrzeja - OPINIE');
+            }
+            if (currentIndex == 1) {
+              return const Text('Najlepsza pizza wg Andrzeja - DODAJ');
+            }
+
+            return const Text('Najlepsza pizza wg Andrzeja - KONTO');
+          },
+        ),
+      ),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return const Center(
-            child: Text('Jeden'),
-          );
+          return RestaurantsPageContent();
         }
         if (currentIndex == 1) {
-          return const Center(
-            child: Text('Dwa'),
-          );
+          return const AddOpinionPageContent();
         }
 
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Jesteś zalogowany jako ${widget.user.email}',
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('Wyloguj się'),
-              ),
-            ],
-          ),
-        );
+        return MyAccountPageContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
@@ -72,5 +68,101 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+}
+
+class MyAccountPageContent extends StatelessWidget {
+  const MyAccountPageContent({
+    super.key,
+    required this.email,
+  });
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Jesteś zalogowany jako $email',
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Text('Wyloguj się'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddOpinionPageContent extends StatelessWidget {
+  const AddOpinionPageContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Dwa'),
+    );
+  }
+}
+
+class RestaurantsPageContent extends StatelessWidget {
+  RestaurantsPageContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream:
+            FirebaseFirestore.instance.collection('restaurants').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text("Loading"));
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Container(
+                    color: Colors.lightBlue,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(document['name']),
+                              Text(document['pizza']),
+                            ],
+                          ),
+                          Text(document['rating'].toString()),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
   }
 }
